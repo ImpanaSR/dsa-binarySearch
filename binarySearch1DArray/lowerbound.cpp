d5c8691665f0821b5fe68e5a5dfe7b37c5dedf74
IMPANASR-20 <vvce23cse0236@vvce.ac.in>
2026-07-07 12:54:16 +0530

    Initial commit

---
+++ binarySearch1DArray/lowerbound.cpp
@@ -0,0 +1,36 @@
+//lower bound -> smallest index such that arr[indx]>=x
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int lowerbound(vector<int> arr, int n, int k){
+    sort(arr.begin(), arr.end());
+    int left=0;
+    int right=n-1;
+    int ans=n;
+
+    while(left<=right){
+        int mid=left+(right-left)/2;
+        
+        if(arr[mid]>=k){
+            ans=mid;
+            right=mid-1;
+        }
+        else
+            left=mid+1;
+    }
+    return ans;
+}
+
+int main(){
+    int n, target;
+     cin>>n;
+    vector<int> arr(n);
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+    cin>>target;
+    int res=lowerbound(arr, n, target);
+    cout<<res;
+    return 0;
+}
